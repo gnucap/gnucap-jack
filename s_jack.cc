e5bda9b8395b41c619bd49073b67b1cb1d4e02d7
Felix Salfelder <felix@salfelder.org>
2021-01-03 00:00:00 +0000

    follow unsigned change in dev-20200626

--- s_jack.cc
+++ s_jack.cc
@@ -121,7 +121,7 @@ void TRANSIENT::options(CS& Cmd)
   bool ploton = IO::plotset  &&  plotlist().size() > 0;
   _sim->_uic = _cold = false;
   _trace = tNONE;
-  unsigned here = Cmd.cursor();
+  size_t here = Cmd.cursor();
   do{ untested();
     ONE_OF
       || Get(Cmd, "c{old}",	   &_cold)
